63ec904c9a83337c64b99d09f20bd6b4ac8aa120
self-use <gdfvsgfd8769@gmail.com>
2024-08-25 15:35:55 +0800

    fix import bootstrap at main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import * as bootstrap from 'bootstrap'
+import 'bootstrap'
 import 'bootstrap/scss/bootstrap.scss'
 
 const app = createApp(App);
-app.use(bootstrap);
+// app.use(bootstrap);
 app.mount('#app');
